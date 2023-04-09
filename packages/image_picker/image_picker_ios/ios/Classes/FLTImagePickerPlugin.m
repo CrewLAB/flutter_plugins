71a6d16ab2dcf50901aafde9a5796e77f52679be
Aidan Lee <aidanwlee@gmail.com>
2023-04-09 10:03:20 -0600

    Remove compression and file type changes form image picker

--- packages/image_picker/image_picker_ios/ios/Classes/FLTImagePickerPlugin.m
+++ packages/image_picker/image_picker_ios/ios/Classes/FLTImagePickerPlugin.m
@@ -229,10 +229,7 @@ typedef NS_ENUM(NSInteger, ImagePickerClassType) { UIImagePickerClassType, PHPic
   UIImagePickerController *imagePickerController = [self createImagePickerController];
   imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
   imagePickerController.delegate = self;
-  imagePickerController.mediaTypes = @[
-    (NSString *)kUTTypeMovie, (NSString *)kUTTypeAVIMovie, (NSString *)kUTTypeVideo,
-    (NSString *)kUTTypeMPEG4
-  ];
+  imagePickerController.mediaTypes = @[(NSString *)kUTTypeMovie];
   imagePickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
   imagePickerController.videoExportPreset = AVAssetExportPresetPassthrough;
 
@@ -240,6 +237,8 @@ typedef NS_ENUM(NSInteger, ImagePickerClassType) { UIImagePickerClassType, PHPic
     NSTimeInterval max = [maxDurationSeconds doubleValue];
     imagePickerController.videoMaximumDuration = max;
   }
+  imagePickerController.videoExportPreset = AVAssetExportPresetPassthrough;
+
 
   self.callContext = context;
 
